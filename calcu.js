f7d96b91ec9dfcae681102b18dcc3c178945c51d
gersonmoreno04 <gersonmoreno18@aragon.unam.mx>
2025-09-03 15:25:19 -0600

    Add files via upload

---
+++ calcu.js
@@ -0,0 +1,12 @@
+//MORENO ANAYA GERSON 
+//CALCULADORA 
+console.log("CALCULADORA");
+let num1= parseInt(prompt("INGRESA NUMERO 1: "));//ingresar numero 1 y 2
+let num2= parseInt(prompt("INGRESA NUMERO 2: "));
+//Resultados
+console.log("Numeros Ingresados: "+num1+","+num2);
+console.log("suma : "+( num1+num2));
+console.log("resta : "+( num1-num2));
+console.log("multiuplicacion : "+( num1*num2));
+console.log("division : "+( num1/num2));
+console.log("modulo : "+( num1%num2));
---
+++ compuerta.js
@@ -0,0 +1,23 @@
+//MORENO ANAYA GERSON
+//COMPUERTAS OR Y AND
+console.log("COMPUERTAS: ");
+console.log("COMPUERTA AND: ");
+console.log("| a | b | S |");
+var and1 = 1 < 0 || 0 > 1; //Hacemos la tabla de verdad de nuestra compuerta AND
+console.log("| 0 | 0 |"+and1+"|");
+var and1 = 1 < 0 || 0 > 1;
+console.log("| 0 | 1 |"+and1+"|");
+var and1 = 1 < 0 || 0 > 1;
+console.log("| 1 | 0 |"+and1+"|");
+var and1 = 1 < 0 || 0 < 1;
+console.log("| 1 | 1 |"+and1+"|");
+console.log("COMPUERTA OR: ");
+var or = 1 < 0 || 0 > 1; //Hacemos la tabla de verdad de nuestra compuerta OR
+console.log("| 0 | 0 |"+or+"|");
+var or = 1 < 0 || 0 < 1;
+console.log("| 0 | 1 |"+or+"|");
+var or = 1 < 0 || 0 < 1;
+console.log("| 1 | 0 |"+or+"|");
+var or = 1 < 0 || 0 < 1;
+console.log("| 1 | 1 |"+or+"|");
+//aprende uml en 24 hrs
