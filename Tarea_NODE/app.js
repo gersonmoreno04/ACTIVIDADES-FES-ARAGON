84aef5dd3d1f87463234e3fae40fbb41b1d75da7
gersonmoreno04 <gersonmoreno18@aragon.unam.mx>
2025-11-08 13:08:50 -0600

    modificaciones a los js de node

--- Tarea_NODE/app.js
+++ Tarea_NODE/app.js
@@ -1,13 +1,10 @@
+//MORENO ANAYA GERSON APP RAIZ DE NODE
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000; // Usaremos un solo puerto para todo
-
-// *** LA LÍNEA MÁGICA ***
-// Esto "publica" la carpeta 'public' y todo su contenido
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Inicia el servidor
+const PORT = 3000; //Usamos el puerto 3000 
+app.use(express.static(path.join(__dirname, 'public'))); //Usamos la carpeta public
+//Inicializa el servidor
 app.listen(PORT, () => {
     console.log(`Servidor principal escuchando en http://localhost:${PORT}`);
 });
